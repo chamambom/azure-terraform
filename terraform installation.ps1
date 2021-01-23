748d5aba168ad1b5a4d3022c0541816bd90ed6d9
Martin Chamambo <Martin.Chamambo@liquidtelecom.com>
2021-01-23 02:35:03 +0200

    .gitignore is now working

---
+++ terraform installation.ps1
@@ -0,0 +1,8 @@
+﻿
+
+terraform -version #run this to verify if terraform installed correctly https://stackoverflow.com/questions/1618280/where-can-i-set-path-to-make-exe-on-windows
+az version #verify if you have installed azure cli correctly , not to be confused with Aazure Az modules https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-windows?tabs=azure-cli
+
+
+
+ 
